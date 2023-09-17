4c1036f8d0717429c6db3aa12e33651a4a0194e8
Camila Alves <102634746+CamilaAlves1@users.noreply.github.com>
2023-09-17 20:15:12 -0300

    Add files via upload

---
+++ Atividade2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Curso
+{
+    class Funcionario
+    {
+        public string Nome;
+        public double Salario;
+    }
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Funcionario p1 = new Funcionario();
+            Funcionario p2 = new Funcionario();
+            double media;
+
+            Console.WriteLine("Insira os dados do funcionário 1");
+            Console.WriteLine("Nome:");
+            p1.Nome = Console.ReadLine();
+            Console.WriteLine("Salario:");
+            p1.Salario = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira os dados do funcionário 2");
+            Console.WriteLine("Nome:");
+            p2.Nome = Console.ReadLine();
+            Console.WriteLine("Salario:");
+            p2.Salario = double.Parse(Console.ReadLine());
+
+            media = (p1.Salario + p2.Salario) / 2;
+            Console.WriteLine("A média salarial é de " + media);
+        }
+    }
+}
---
+++ Atividade3.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Curso
+{
+    class Retangulo
+    {
+        public double Altura;
+        public double Largura;
+
+        public double CalculaArea()
+        {
+            return Altura * Largura;
+        }
+
+        public double CalculaPerimetro()
+        {
+            return (Altura * 2) + (Largura * 2);
+        }
+    }
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Retangulo m1 = new Retangulo();
+            Retangulo m2 = new Retangulo();
+
+            Console.WriteLine("Insira a altura do retângulo:");
+            m1.Altura = double.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Insira a largura do retângulo:");
+            m2.Largura = double.Parse(Console.ReadLine());
+
+            double area = m1.CalculaArea();
+            double perimetro = m1.CalculaPerimetro();
+            double diagonal = Math.Sqrt(m1.Altura * m1.Altura + m2.Largura * m2.Largura);
+
+            Console.WriteLine("A área do retângulo é " + area);
+            Console.WriteLine("O perímetro do retângulo é " + perimetro);
+            Console.WriteLine("A diagonal do retângulo é " + diagonal);
+        } 
+    }
+}
